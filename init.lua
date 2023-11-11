3bfbb4ae1b8794d07fbebf53d99e9bd5e7ab9bb4
Skye Kychenthal <34521078+SkyMocha@users.noreply.github.com>
2023-11-11 15:00:17 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,32 @@
+premiereProName = "Adobe Premiere Pro 2024"
+
+function bindHotkeyOnlyTo(appname, modifier, key, message, callback, ...)
+    hotkeyHandler = hs.hotkey.bind(modifier, key, message, function()
+
+        local currentApp = hs.application.frontmostApplication():name()
+
+        if (appname == currentApp) then
+            callback()
+        else
+            hotkeyHandler:disable()
+            hs.eventtap.keyStroke(modifier, key)
+            hotkeyHandler:enable()
+        end
+    end)
+end
+
 -- RIPPLE DELETE @ PLAYHEAD --
 -- Unbind cmd shift D
 -- Bind D playback head
 -- Bind Shift D ripple delete playhead
-hs.hotkey.bind({"cmd", "shift"}, "D", function()
-    -- hs.alert.show("ripple delete at playhead")
-    hs.application.launchOrFocus("Adobe Premiere Pro 2024")
+bindHotkeyOnlyTo(premiereProName, {"cmd", "shift"}, "D", nil, function()
     hs.eventtap.keyStroke({}, 'D', hs.application.frontmostApplication())
     hs.eventtap.keyStroke({'shift'}, 'D', hs.application.frontmostApplication())
 end)
 
 -- CHANGE VOLUME +3 DB @ PLAYHEAD --
 -- Remap G to Shift G
-hs.hotkey.bind({'shift', 'cmd'}, 'G', function()
-    hs.application.launchOrFocus("Adobe Premiere Pro 2024")
+bindHotkeyOnlyTo(premiereProName, {'shift', 'cmd'}, 'G', nil, function()
     hs.eventtap.keyStroke({}, 'D', hs.application.frontmostApplication())
     hs.eventtap.keyStroke({'shift'}, 'G', hs.application.frontmostApplication())
     hs.eventtap.keyStroke({}, '3', hs.application.frontmostApplication())
@@ -21,6 +35,5 @@ end)
 
 -- RELOAD HAMMERSPOON --
 hs.hotkey.bind({'shift', 'cmd'}, 'R', function()
-    hs.alert.show("reload")
     hs.reload()
 end)
